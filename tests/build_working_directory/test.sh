86efce706d8f99b523b5fa4d731ba421155203c5
Laurenz <lalten@users.noreply.github.com>
2026-04-03 19:01:19 +0200

    Fix $PATH of bwd test for Debian (#409)
    On Debian 11 (non-merged-usr), env and sort are in `/usr/bin`, not `bin`

--- tests/build_working_directory/test.sh
+++ tests/build_working_directory/test.sh
@@ -7,13 +7,13 @@ actual_wd="$(pwd)"
 unset BUILD_WORKING_DIRECTORY
 
 # BUILD_WORKING_DIRECTORY is set inside the appimage when libfuse-mounted
-env="$(env -i -- PATH="/bin" tests/build_working_directory/test.appimage)"
+env="$(env -i -- PATH="/bin:/usr/bin" tests/build_working_directory/test.appimage)"
 grep -q OWD= <<<"$env"
 bwd="$(grep BUILD_WORKING_DIRECTORY= <<<"$env" | cut -d= -f2-)"
 [ "$bwd" = "$actual_wd" ]
 
 # BUILD_WORKING_DIRECTORY is set inside the appimage when using APPIMAGE_EXTRACT_AND_RUN
-env="$(env -i -- PATH="/bin" APPIMAGE_EXTRACT_AND_RUN=1 tests/build_working_directory/test.appimage)"
+env="$(env -i -- PATH="/bin:/usr/bin" APPIMAGE_EXTRACT_AND_RUN=1 tests/build_working_directory/test.appimage)"
 grep -q OWD= <<<"$env" && exit 1 # https://github.com/AppImage/type2-runtime/issues/23
 bwd="$(grep BUILD_WORKING_DIRECTORY= <<<"$env" | cut -d= -f2-)"
 [ "$bwd" = "$actual_wd" ]
