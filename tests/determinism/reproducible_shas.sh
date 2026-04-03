2836d67e8f791b5724fe2be5ca339d34a37a1fe6
lalten <git@laure.nz>
2026-04-03 18:25:22 +0200

    upload files on failure

--- tests/determinism/reproducible_shas.sh
+++ tests/determinism/reproducible_shas.sh
@@ -7,10 +7,11 @@
 set -euxo pipefail
 
 query='kind("appimage rule", //...)'
-tempdir="$(mktemp -d)"
+tempdir="${1:-/tmp/determinism-test}"
+rm -rf "$tempdir"
+mkdir -p "$tempdir"
 targets="$tempdir/targets"
 files="$tempdir/files"
-trap 'rm -rf "$tempdir"' EXIT
 
 bazel query "$query" >"$targets"
 bazel cquery "$query" --output files >"$files"
@@ -19,10 +20,14 @@ bazel clean
 bazel build --remote_cache= --disk_cache= --target_pattern_file="$targets" --platforms=//:linux_x86_64
 xargs --arg-file="$files" sha256sum | tee "$tempdir/shas0.txt"
 
-xargs --arg-file="$files" rm -f
+mkdir -p "$tempdir/build0"
+xargs --arg-file="$files" -I{} mv {} "$tempdir/build0/"
 
 bazel clean
 bazel build --remote_cache= --disk_cache= --target_pattern_file="$targets" --platforms=//:linux_x86_64
 xargs --arg-file="$files" sha256sum | tee "$tempdir/shas1.txt"
+
+mkdir -p "$tempdir/build1"
+xargs --arg-file="$files" -I{} cp {} "$tempdir/build1/"
 
 diff -u "$tempdir/shas0.txt" "$tempdir/shas1.txt"
